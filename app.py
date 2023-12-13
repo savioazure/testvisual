feafa792fb77d5951330dfc558a9077e26ca2d52
Savio Kerber <savioazure@gmail.com>
2023-12-13 18:51:17 +0100

    2nd test

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from screen_manager import ScreenManager
 def run():
     while True:
         if ScreenManager.is_app_focused(app_title_name="linkedin"):
-            coordinates = ScreenManager.search_image_on_screen(image_to_search="./testingcut.png")
+            coordinates = ScreenManager.search_image_on_screen(image_to_search="./testingcut2.png")
             for coordinate in coordinates:
-                print(coordinate)
+                print()
 
             
 run()
