c6f82b0ac9e9686ee520bfcee07aa07e7c3718fa
Savio Kerber <savioazure@gmail.com>
2023-12-14 01:11:24 +0100

    app working

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
+import random
+import time
+import keyboard
+
+
 from screen_manager import ScreenManager
+
 
 def run():
     while True:
         if ScreenManager.is_app_focused(app_title_name="linkedin"):
             coordinates = ScreenManager.search_image_on_screen(image_to_search="./testingcut3.png")
-            for coordinate in coordinates:
-                print()
-
             
+            for coordinate in coordinates:
+#                print(coordinate)
+                ScreenManager.click_on_screen(coordinate_to_click=coordinate)
+                keyboard.press_and_release('TAB, TAB, TAB, Enter')
+
+                time.sleep(random.randint(1, 2))
+            
+            
+                
+            ScreenManager.scroll_on_screen(value_to_scroll=-400)
+            time.sleep(random.randint(1, 2))
+    
+            if keyboard.is_pressed("ESC"):
+                print("saliendo del programa Lnkbot")
+                break
+        
+        
 run()
+
+"""    dondeclicares = ScreenManager.search_image_on_screen(image_to_search="./unfollow.png")
+            for dondeclicar in dondeclicares:
+                ScreenManager.click_on_screen(coordinate_to_click=dondeclicar) """
--- screen_manager.py
+++ screen_manager.py
@@ -11,7 +11,16 @@ class ScreenManager:
     @staticmethod
     def is_app_focused(app_title_name: str):
         active_window_title = win32gui.GetWindowText(win32gui.GetForegroundWindow()).lower()
+        #print("\n\n" + active_window_title)
         return app_title_name in active_window_title
+    
+    @staticmethod
+    def click_on_screen(coordinate_to_click: int):
+        pyautogui.click(coordinate_to_click)
+        
+    @staticmethod
+    def scroll_on_screen(value_to_scroll: int):
+        pyautogui.scroll(value_to_scroll)
 
     @staticmethod
     def search_image_on_screen(image_to_search: str):
